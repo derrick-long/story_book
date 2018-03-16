a1c2b88fabbd70987ed931f5f9465d9a30d135a7
derrick-long <long.derrick8@gmail.com>
2018-03-16 19:10:10 -0400

    add passport and google oauth20

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const mongoose = require('mongoose');
-
+const passport = require('passport');
 const app = express();
 
 const port = process.env.PORT || 5000;
+//passport config
+require('./config/passport')(passport);
+//load routes
+const auth = require('./routes/auth');
 
 app.get('/', (req, res)=> {
   res.send('up and running!');
 });
+//routes
+
+app.use('/auth', auth);
+
+
+
+
 
 
 app.listen(port, ()=>{
