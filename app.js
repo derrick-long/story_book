a350b71329f5cfa0e5ba706afb395ab1a53e8b9a
derrick-long <long.derrick8@gmail.com>
2018-03-16 23:25:47 -0400

    add user model schema

--- app.js
+++ app.js
@@ -4,10 +4,27 @@ const passport = require('passport');
 const app = express();
 
 const port = process.env.PORT || 5000;
+
 //passport config
+
 require('./config/passport')(passport);
+
 //load routes
+
 const auth = require('./routes/auth');
+
+//load keys
+const keys = require('./config/keys');
+//mongoose connect
+mongoose.Promise = global.Promise;
+
+mongoose.connect(keys.mongoURI,{
+
+})
+  .then(() => console.log('MongoDB connected'))
+  .catch(err=> console.log(err));
+
+
 
 app.get('/', (req, res)=> {
   res.send('up and running!');
--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-  mongoURI:'mongodb://Derrick:Derrick1@ds019058.mlab.com:19058/story-book-dev',
+  mongoURI:'mongodb://Derrick:Derrick246@ds019058.mlab.com:19058/story-book-dev',
   googleClientID:'145874568032-rlt3f81fe4a5ovql0c95lkqcqhkoseri.apps.googleusercontent.com',
   googleClientSecret: 'nzLWkz2ld99FNaEDXFLVvbW0'
 };
