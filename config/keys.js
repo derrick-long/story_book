045efc90b02b333679292f1b86a5ba70c7745b90
derrick-long <long.derrick8@gmail.com>
2018-03-16 22:03:56 -0400

    add file

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:'mongodb://Derrick:Derrick1@ds019058.mlab.com:19058/story-book-dev',
+  googleClientID:'145874568032-rlt3f81fe4a5ovql0c95lkqcqhkoseri.apps.googleusercontent.com',
+  googleClientSecret: 'nzLWkz2ld99FNaEDXFLVvbW0'
+};
