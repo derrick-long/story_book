25825c705087e6f37831b5f75ea96a1e60d00414
derrick-long <long.derrick8@gmail.com>
2018-03-20 10:15:23 -0400

    fix db link to mlabs

--- config/passport.js
+++ config/passport.js
@@ -1,21 +1,21 @@
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const mongoose = require('mongoose');
 const keys = require('./keys');
-
-//load user model
-
-const user = mongoose.model('users');
+// Load user model
+const User = mongoose.model('users');
 
 module.exports = function(passport){
   passport.use(
     new GoogleStrategy({
       clientID: keys.googleClientID,
-      clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback',
+      clientSecret:keys.googleClientSecret,
+      callbackURL:'/auth/google/callback',
       proxy: true
-    }, (accessToken, refreshToken, profile, done)=> {
-      const image = profile.photos[0].value.substring(0,
-      profile.photos[0].value.indexOf('?'));
+    }, (accessToken, refreshToken, profile, done) => {
+      // console.log(accessToken);
+      // console.log(profile);
+
+      const image = profile.photos[0].value.substring(0, profile.photos[0].value.indexOf('?'));
 
       const newUser = {
         googleID: profile.id,
@@ -24,13 +24,16 @@ module.exports = function(passport){
         email: profile.emails[0].value,
         image: image
       };
-      //check for existing user
+
+      // Check for existing user
       User.findOne({
         googleID: profile.id
-      }).then(user=> {
+      }).then(user => {
         if(user){
+          // Return user
           done(null, user);
         } else {
+          // Create user
           new User(newUser)
             .save()
             .then(user => done(null, user));
